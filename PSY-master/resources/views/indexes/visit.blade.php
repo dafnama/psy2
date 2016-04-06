ea95b88f96e1936408a41bdeac198806975a3ddb
inbalkiv <inbalkiv@post.bgu.ac.il>
2016-04-06 22:38:29 +0300

    fix visit and add filters

--- PSY-master/resources/views/indexes/visit.blade.php
+++ PSY-master/resources/views/indexes/visit.blade.php
@@ -1,10 +1,95 @@
 @extends('app')
-
+<?php use App\Models\Match;
+use App\Models\Psychologist;
+use App\Models\Institute;?>
 @section('page-title')
     <h1>ביקורים</h1>
 @stop
 
 @section('content')
+<?php $sum_hour=0;?>
+    <form class="psy-form"  method="GET">
+        <input type="hidden" name="_method" value="post">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+        <h4>סנן לפי:</h4>
+        <span>פסיכולוג:</span>
+                <span class="input-line">
+                    <select name="filter_psy" class=" mult">
+                        <option disabled="disabled" selected="selected" value="">בחר מרשימה</option>
+                        <?php $psy_array=array();?>
+
+                        @foreach ($visits as $visit)
+                            <?php 
+                            if($visit->match_id){
+                                $psy=Psychologist::find(Match::find($visit->match_id)->psychologist_id);
+                                if (!in_array($psy,$psy_array )){
+                                    $psy_array[]=$psy;}
+                            }
+                            ?>
+                        @endforeach
+                        @foreach ($psy_array as $psy)
+                            <option value="{{{$psy->id}}}">{{{$psy->last_name .(' ').$psy->first_name }}}</option>
+                        @endforeach
+                    </select>
+                </span>
+        
+              <span>מוסד:</span>
+                <span class="input-line">
+                    <select name="filter_institute" class=" mult">
+                        <option disabled="disabled" selected="selected" value="">בחר מרשימה</option>
+                        <?php $inst_array=array();?>
+                        @foreach ($visits as $visit)
+                            <?php 
+                            if($visit->match_id){
+                                $inst=Institute::find(Match::find($visit->match_id)->institute_id);
+                                if (!in_array($inst,$inst_array )){
+                                    $inst_array[]=$inst;
+                                }
+                            }
+                            ?>
+                        @endforeach
+                        @foreach ($inst_array as $insti)
+                            <option value="{{{$insti->id}}}">{{{$insti->name }}}</option>
+                        @endforeach
+                    </select>
+                </span>
+        
+  
+
+         <span>פעילות:</span>
+                <span class="input-line ">
+                    <select name="filter_activity" class=" mult">
+                        <option disabled="disabled" selected="selected" value="">בחר מרשימה</option>
+                        <?php $kinds=array();?>
+                        @foreach ($visits as $visit)
+                            <?php if (!in_array($visit->activity, $kinds)){
+                                $kinds[]=$visit->activity;
+                            }
+                            ?>
+                        @endforeach
+                        @foreach ($kinds as $kind)
+                            <option value="{{{$kind}}}">{{{$kind}}}</option>
+                        @endforeach
+                    </select>
+                </span>
+         <br>
+         <br>
+        <span>מתאריך:</span>
+                <span class="input-line">
+                     <input type="date" id="datepicker" class="datepicker" size="10" name="filter_date" >
+                </span>
+
+        <span>עד תאריך:</span>
+                <span class="input-line">
+                     <input type="date" id="datepicker" class="datepicker" size="10" name="filter_to_date" >
+                </span>
+
+        <span>
+                <button type="submit" class="pull-left approve">שלח</button>
+        </span>
+    </form> <!-- /form -->
+
 
     <table border="1">
          <thead>
@@ -13,6 +98,7 @@
             <td>מחק</td>
             <td>מזהה</td>
             <td>מוסד</td>
+            <td>פסיכולוג</td>
             <td>תאריך</td>
             <td>פעילות</td>
             <td>משך הביקור</td>
@@ -39,14 +125,27 @@
                 </td>
                 <td>{{$vis->id}}</td>
                 <td>{{{$vis->intitute_name_const}}}</td>
+               <?php if($vis->match_id){
+                   $psychologist=Psychologist::find(Match::find($vis->match_id)->psychologist_id);
+                   $psy=$psychologist->first_name.' ' .$psychologist->last_name;
+               }
+               else{
+                   $psy=null;
+               }
+                ?>
+                <td>{{{$psy }}}</td>
                 <td>{{{$vis->date}}}</td>
                 <td>{{{$vis->activity}}}</td>
                 <td>{{{$vis->length}}}</td>
                 <td>{{{$vis->comment}}}</td>
             </tr>
+            <?php $sum_hour=$sum_hour+$vis->length ;?>
         @endforeach
     </table>
-
+<div><?php if(isset($visits) && $visits){echo $visits->count() ." ";}?>רשומות
+    ,
+סה"כ שעות: 
+<?php echo $sum_hour?></div>
 
 
 
